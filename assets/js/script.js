7965332acb22628c0f02ff8d2a80964004e9bbb8
Kodi <120777232+kowdeegithub@users.noreply.github.com>
2025-12-18 02:12:08 +0800

    Update: i just update the error of the navbar position

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
    }
-};
+}; 
 
 
 /**
@@ -30,4 +30,17 @@ const addEventOnElements = function (elements, eventType, callback) {
  */
 
 const navbar = document.querySelector("[data-navbar]");
-const navtogglers = document.querySelectorAll("[data-nav-toggler]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+/**
+ * NAVBAR 
+ */
