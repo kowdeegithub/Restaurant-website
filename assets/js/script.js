5aff2e2ea04f37ba1cfa07dad863bdf6711e2be0
Kodi <120777232+kowdeegithub@users.noreply.github.com>
2025-12-17 20:04:18 +0800

    Update: finish design the menu bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,22 @@ window.addEventListener("load", function () {
   preloader.classList.add("loaded");
   document.body.classList.add("loaded");
 });
+
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+   }
+};
+
+
+/**
+ * NAVBAR 
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navtogglers = document.querySelectorAll("[data-nav-toggler]");
